0faaf2d9bca4fee2780e9f20556ae040ecb7a7bf
Getabicha Daniel <getabicha.weldekidan@gmail.com>
2022-05-18 21:43:12 +0300

    focus on the Exersise

---
+++ function-declare.js
@@ -0,0 +1,20 @@
+/*'function sayHello(){
+    console.log('........');
+    console.log('hello');
+    console.log('........');
+}
+sayHello()
+
+let a=sayHello;
+a();
+a();
+*/
+
+function sayHello(name){
+    console.log('........');
+    console.log('hello ' +  name  +  '  Daniel');
+    console.log('........');
+}
+sayHello('Rina');
+sayHello('Barok');
+sayHello('Baslel');
---
+++ variabels.js
@@ -0,0 +1,6 @@
+let x=7
+ x=4;
+ x=9;
+let y=3;
+let z =x+y;
+console.log('Answer: '+z);
